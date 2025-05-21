f3327f07843bb50ce6d9dc632a930d708cf49132
DigvijayAbhishikth <manojchowdari@gmail.com>
2025-05-22 01:54:08 +0530

    added the sidebar and top bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider, Box } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import ToolBar from "./scenes/Global/TopBar";
+import SideBar from "./scenes/Global/SideBar";
+import Dashboard from "./scenes/dashboard/Dashboard";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [theme, colorMode] = useMode();
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {/* Use flex layout to show Sidebar and content side by side */}
+        <Box display="flex" height="100vh" width="100%">
+          <SideBar />
+          <Box flex="1" display="flex" flexDirection="column">
+            <ToolBar />
+            <Box flex="1" p="20px">
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/team" element={<Dashboard />} />
+                <Route path="/contacts" element={<Dashboard />} />
+                <Route path="/invoices" element={<Dashboard />} />
+                <Route path="/form" element={<Dashboard />} />
+                <Route path="/bar" element={<Dashboard />} />
+                <Route path="/pie" element={<Dashboard />} />
+                <Route path="/line" element={<Dashboard />} />
+                <Route path="/faq" element={<Dashboard />} />
+                <Route path="/geography" element={<Dashboard />} />
+                <Route path="/calendar" element={<Dashboard />} />
+              </Routes>
+            </Box>
+          </Box>
+        </Box>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
-export default App
+export default App;
